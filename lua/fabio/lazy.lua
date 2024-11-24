26ac4ea5db799b2cf22181639ecc7b0fcf8223a8
Fabio Calero <fabiojcalero@gmail.com>
2024-11-23 20:16:40 -0400

    Initial setup complete

--- lua/fabio/lazy.lua
+++ lua/fabio/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("fabio.plugins", {
+require("lazy").setup({ { import = "fabio.plugins" }, { import = "fabio.plugins.lsp" } }, {
     checker = {
         enabled = true,
         notify = false,
